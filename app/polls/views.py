df66443318ae0c2e056c167dc5f18cd478880f33
Stiff228 <geka.stiffler@gmail.com>
2022-07-21 18:16:59 +0300

    Update views.py

--- app/polls/views.py
+++ app/polls/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 def index(request):
     context = {
         'title': 'Главная страница',
-        'info1': 'Инфо текст HelloWorldы',
-        'info2': 'Инфо текст HelloWorldы'
+        'info1': 'Инфо текст HelloWorldыыы',
+        'info2': 'Инфо текст HelloWorldыыы'
 
     }
     return render(request, 'polls/main.html', context=context)
